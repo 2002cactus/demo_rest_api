509c06dfa728824602cb3a6345743520ef3e56f9
Infyinfy <125547553+2002cactus@users.noreply.github.com>
2025-02-17 18:29:16 +0700

    Update response.py

--- response.py
+++ response.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, jsonify
 
-app = Flask(__name__)
+response = Flask(__name__)
 
 users = []  # Danh sách user lưu trong RAM (tạm thời)
 
-@app.route('/users', methods=['POST'])
+@response.route('/users', methods=['POST'])
 def add_user():
     data = request.json
     if "name" in data and "email" in data:
@@ -13,9 +13,9 @@ def add_user():
     else:
         return jsonify({"error": "Missing data!"}), 400
 
-@app.route('/users', methods=['GET'])
+@response.route('/users', methods=['GET'])
 def get_users():
     return jsonify(users)  # Trả về danh sách user đã gửi
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    response.run(debug=True)
